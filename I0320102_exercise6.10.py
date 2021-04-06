852cbe10728b0d77529f7907d2ef5cf98deabf0c
Titus Kurniawan Sandy <titussandy11@gmail.com>
2021-04-06 19:30:13 +0700

    Exercise 6.10

--- I0320102_exercise6.10.py
+++ I0320102_exercise6.10.py
@@ -1,1 +1,12 @@
+#Titus Kurniawan Sandy Purwanto
+#I0320102
+#kelas c
 
+i = 1
+while i <= 10 :
+    j = 1
+    while j <= 1 :
+        print('%d ' % (i*j), end = '' )
+        j =+ 1
+    print()
+    i =+ 1
