73b28b52da3cdb5673c55ecc09e227ae4f14e5c7
Titus Kurniawan Sandy <titussandy11@gmail.com>
2021-04-06 19:40:31 +0700

    Exercise 6.13

--- I0320102_exercise6.13.py
+++ I0320102_exercise6.13.py
@@ -1,1 +1,12 @@
+#Titus Kurniawan Sandy Purwanto
+#i0320102
+#Kelas c
 
+def tambah(a, b) :
+    c = a+b
+    return c
+x = int(input('Masukkan bilangan ke 1 : '))
+y = int(input('Masukkan bilangan ke 2 : '))
+
+hasil = tambah(x,y)
+print(' %d + %d = %d' %(x,y, hasil))
