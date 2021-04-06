328063a0ff9184cc0c80fed12d237b109a764fd6
Titus Kurniawan Sandy <titussandy11@gmail.com>
2021-04-06 19:05:08 +0700

    Exercise 6.2

--- I0320102_exercise6.2.py
+++ I0320102_exercise6.2.py
@@ -1,1 +1,12 @@
+#Titus Kurniawan Sandy Purwanto
+#I0320102
+#Kelas C
 
+#input untuk nilai n
+n = int(input('Masukkan banyak pengulangan : '))
+
+#melakukan pengulangan
+i = 1
+while i <= n :
+    print(i)
+    i = i + 1
