4de34a110ee7e0259ed9afd319fdcbd3e699d08c
Titus Kurniawan Sandy <titussandy11@gmail.com>
2021-04-06 18:56:48 +0700

    Exercise 6.1

--- I0320102_exercise6.1.py
+++ I0320102_exercise6.1.py
@@ -1,1 +1,13 @@
+#Titus Kurniawan Sandy Purwanto
+#I0320102
+#Kelas C
 
+#menentukan banyak pengulangan
+print('Nomor :')
+n = 5
+
+#melakukan pengulangan
+i = 1
+while i <= n:
+    print(i)
+    i += 1
