683ed9106e136cf8fe7b3e844c31b8a04e64d416
Titus Kurniawan Sandy <titussandy11@gmail.com>
2021-04-06 20:17:10 +0700

    Soal 2

--- I0320102_soal2_tugas6.py
+++ I0320102_soal2_tugas6.py
@@ -1,1 +1,17 @@
+#Titus Kurniawan Sandy Purwanto
+#I0320102
+#kelas c
 
+print('-----Program Penghitung Rata-rata-----')
+
+print('================')
+x = float(input('Input banyaknya nilai : '))
+nilai = []
+i = 1
+while i <= x:
+    score = float(input("Masukkan nilai anda : "))
+    nilai.append(score)
+    i = i + 1
+
+rata2_nilai = sum(nilai)/x
+print("Nilai rata-rata anda adalah ", rata2_nilai)
