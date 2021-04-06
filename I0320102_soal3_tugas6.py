bf77ab5fe7953cddff0649662bb5bec69e015159
Titus Kurniawan Sandy <titussandy11@gmail.com>
2021-04-06 23:26:11 +0700

    Soal 3

--- I0320102_soal3_tugas6.py
+++ I0320102_soal3_tugas6.py
@@ -1,1 +1,11 @@
+#Titus Kurniawan Sandy Purwanto
+#I0320102
+#Kelas C
 
+for i in range(10,25) :
+        for j in range(2,i):
+            if (i % j) == 0:
+                print(i, 'bukan prima')
+                break
+        else:
+            print(i, 'adalah bilangan prima')
